2e41b2e0edf43b90d62f2e7e415b37462fd854fb
slow-connect <louisdeberlin+github@gmail.com>
2023-12-01 06:38:51 +0100

    Completed day 1

--- day01.py
+++ day01.py
@@ -1,3 +1,34 @@
 import aoc
+import re
+data = aoc.get_lst(1)
+num = []
+numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-data = aoc.get_int(1)
+# test = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+# test = data.split('\n')
+
+def two_digit_1(str):
+    num = ''
+    for i in range(len(str)):
+        if str[i] in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}:
+            num += str[i]
+            break
+    for i in range(len(str)-1, -1, -1):
+        if str[i] in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}:
+            num += str[i]
+            break
+    if len(num) == 0:
+        return 0
+    else:
+        return int(num)
+
+def two_digit_2(str):
+    for k in range(len(numbers)):
+        p = re.compile(numbers[k])
+        str = str.replace(numbers[k], numbers[k] + k.__str__() + numbers[k])
+    return two_digit_1(str)
+
+for i in range(len(data)):
+    num.append(two_digit_2(data[i]))
+
+print(sum(num))
