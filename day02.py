793ffbe4d36eff7a11268744575cd180b0c70320
slow-connect <louisdeberlin+github@gmail.com>
2023-12-02 12:39:33 +0100

    Update day02.py

--- day02.py
+++ day02.py
@@ -3,10 +3,8 @@ import re
 
 data = aoc.get_lst(2)
 # data = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n".split('\n')
-red, green, blue = 12, 13, 14
 re_str = r"Game \d*: "
 re_pattern = r"[0-9]* (green|blue|red)"
-
 
 def p2(re_str, re_pattern):
     cnt = 0
